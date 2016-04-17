6ce6784b005aed02197c173ed9ae5290c08e43bd
Terrance Niechciol <TNiechciol@gmail.com>
2016-04-16 22:10:29 -0400

    Load and search history file directly

--- asdf.c
+++ asdf.c
@@ -6,8 +6,39 @@
 
 #include <stdio.h>
 
+const int MAX_HISTORY_LINE_SIZE=300;
+
 bool isWhitespace(char c) {
   return c == ' ' || c == '\0';
+}
+
+char* loadHistoryData(int* numLines) {
+  char* history_data;
+
+  FILE* history_file = fopen(getenv("HISTFILE"), "r");
+  if (history_file != NULL) {
+    int nLines = 0;
+    {
+      char line[MAX_HISTORY_LINE_SIZE];
+      while(fgets((char*)(&line), MAX_HISTORY_LINE_SIZE*sizeof(char), history_file) != NULL) {
+        nLines += 1;
+      }
+    }
+
+    rewind(history_file);
+
+    {
+      history_data = malloc(nLines*MAX_HISTORY_LINE_SIZE*sizeof(char));
+      for (int i = 0; i < nLines; i++) {
+        fgets(history_data+i*MAX_HISTORY_LINE_SIZE, MAX_HISTORY_LINE_SIZE*sizeof(char), history_file);
+      }
+    }
+
+    *numLines = nLines;
+  }
+  fclose(history_file);
+
+  return history_data;
 }
 
 int main() {
@@ -17,10 +48,15 @@ int main() {
 
   const int BUFFER_SIZE = 150;
 
-  char* buffer = malloc(BUFFER_SIZE*sizeof(char));
-  buffer[0] = '\0';
+  char* input = malloc(BUFFER_SIZE*sizeof(char));
+  input[0] = '\0';
   int size = 0;
   int cursor = 0;
+
+  int numLines;
+  char* historyData = loadHistoryData(&numLines);
+
+  int selected = 0;
 
   while(1) {
     int c = getch();
@@ -29,7 +65,7 @@ int main() {
       case KEY_DC:
       case 127:
         if (size > 0) {
-          memmove(buffer+cursor-1, buffer+cursor, (size-cursor+1)*sizeof(char));
+          memmove(input+cursor-1, input+cursor, (size-cursor+1)*sizeof(char));
           size--;
           if (cursor > 0) {
             cursor--;
@@ -46,9 +82,17 @@ int main() {
           cursor++;
         }
         break;
+      case KEY_UP:
+        if (selected > 0) {
+          selected--;
+        }
+        break;
+      case KEY_DOWN:
+        selected++;
+        break;
       case 562:
         cursor++;
-        while (cursor < size && !(!isWhitespace(buffer[cursor]) && isWhitespace(buffer[cursor+1]))) {
+        while (cursor < size && !(!isWhitespace(input[cursor]) && isWhitespace(input[cursor+1]))) {
           cursor++;
         }
         cursor++;
@@ -58,7 +102,7 @@ int main() {
         break;
       case 547:
         cursor -= 2;
-        while (cursor > 0 && !(isWhitespace(buffer[cursor]) && !isWhitespace(buffer[cursor+1]))) {
+        while (cursor > 0 && !(isWhitespace(input[cursor]) && !isWhitespace(input[cursor+1]))) {
           cursor--;
         }
         if (cursor > 0) {
@@ -69,25 +113,51 @@ int main() {
         }
         break;
       default:
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ') {
+        if ((c >= 'a' && c <= 'z')
+         || (c >= 'A' && c <= 'Z')
+         || c == ' '
+         || c == '-'
+         || c == '.'
+         || c == '*'
+         || c == '\\') {
           if (size < BUFFER_SIZE-1) {
-            memmove(buffer+cursor+1, buffer+cursor, (size-cursor+1)*sizeof(char));
-            buffer[cursor] = c;
+            memmove(input+cursor+1, input+cursor, (size-cursor+1)*sizeof(char));
+            input[cursor] = c;
             size++;
             cursor++;
           }
         }
-        //sprintf(buffer, "Num: %d", c);
+        //sprintf(input, "Num: %d", c);
         break;
     }
 
     clear();
 
-    mvprintw(0, 0, buffer);
+    int max_y;
+    int max_x;
+    getmaxyx(stdscr, max_y, max_x);
+
+    mvprintw(0, 0, input);
+
+    for (int i = 0; i < max_x; i++) {
+      mvprintw(1, i, "-");
+    }
+
+    int line = 2;
+    for (int i = numLines; i >= 0 && line < 50; i--) {
+      mvprintw(line, 0, " ");
+      if (strstr(historyData+i*MAX_HISTORY_LINE_SIZE, input) != NULL) {
+        mvprintw(line, 1, historyData+i*MAX_HISTORY_LINE_SIZE);
+        line++;
+      }
+    }
+    mvprintw(selected+2, 0, ">");
+
     move(0, cursor);
   }
 
-  free(buffer);
+  free(historyData);
+  free(input);
 
   endwin();
 
